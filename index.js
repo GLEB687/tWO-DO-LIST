5ec1517b53302b3aa189e4a516b86c9a0d670462
GLEB687 <glebzlun@mail.ru>
2026-04-07 19:58:45 +0300

    7.04

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
+const task = document.getElementById("task-list")
+const bt = document.getElementById("add-btn")
+const IT = document.getElementById("imput-task")
+bt.addEventListener("click", addT)
+
+let tap = [
+    {
+        title: 'ничего не делать',
+        priority: 'HayM',
+        isCompleted: false,
+    },
+    {
+        title: 'купить куртку',
+        priority: ' HayL',
+        isCompleted: false,
+    },
+];
+
 function start() {
     console.log("привет");
     alert("111111")
 }
-const task = document.getElementById("task-list")
-const bt = document.getElementById("add-btn")
-const IT = document.getElementById("imput-task")
-//bt.onclick = start
 
-bt.addEventListener("click", addT)
+tap.forEach( x => createTaskElement(x))
+
+document.addEventListener('keydown',
+    (ev) => {
+        if (ev.key === 'Enter') {
+            addT()
+        }
+    })
 
 function addT() {
     const TT = IT.value;
     if (TT) {
-        const NT = document.createElement("li")
-        NT.classList.add("taskp")
-        NT.classList.add("HayL")
-        NT.addEventListener('click', (ev) => {
-            ev.currentTarget.classList.toggle('complet');
-        });
-        NT.innerHTML = `
-    <span class="task-content">${TT}</span>
+
+        IT.value = ""
+    }
+}
+
+function createTaskElement(FFF) {
+    const NT = document.createElement("li")
+    NT.classList.add("taskp")
+    NT.classList.add("HayL")
+    NT.addEventListener('click', (ev) => {
+        ev.currentTarget.classList.toggle('complet');
+    });
+    NT.innerHTML = `
+    <span class="task-content">${FFF.title}</span>
     <div class="task-a">
         <button class="task-bth" onclick="this.parentNode.parentNode.remove()"><span
                 class="material-symbols-outlined">
@@ -27,9 +54,5 @@ function addT() {
             </span>
         </button>
     </div>`
-        task.append(NT)
-        IT.value = ""
-    }
-    
-
+    task.append(NT)
 }
